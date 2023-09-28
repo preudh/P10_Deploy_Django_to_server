e87f8691d70077ff153d33e6157fe0f8ab40febe
Patrice Preudhomme <preudhomme.patrice@gmail.com>
2023-09-28 16:39:45 +0200

    test_settings.py

---
+++ P8_Django_Purbeurre/test_settings.py
@@ -0,0 +1,13 @@
+from P8_Django_Purbeurre.settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
