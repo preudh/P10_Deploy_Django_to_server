ea30d9d4f16f95e2156e0472aa76e9e9ea8f81e5
Patrice Preudhomme <preudhomme.patrice@gmail.com>
2023-09-29 08:09:29 +0200

    secret keys

--- P8_Django_Purbeurre/test_settings.py
+++ P8_Django_Purbeurre/test_settings.py
@@ -1,5 +1,8 @@
 from P8_Django_Purbeurre.settings import *
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")  # take secret key from environment variables
+
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
