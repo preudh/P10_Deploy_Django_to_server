e873deca743a353e015ca8c0c27bcf661ff07669
Patrice Preudhomme <preudhomme.patrice@gmail.com>
2023-10-27 10:42:47 +0200

    cicd30

--- P8_Django_Purbeurre/settings.py
+++ P8_Django_Purbeurre/settings.py
@@ -88,46 +88,61 @@ WSGI_APPLICATION = 'P8_Django_Purbeurre.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # Database configuration
-docker_desktop = os.environ.get('DJANGO_ENV') == 'docker_desktop'
-cicd = os.environ.get('DJANGO_ENV') == 'cicd'
-local = os.environ.get('DJANGO_ENV') == 'local'
+# docker_desktop = os.environ.get('DJANGO_ENV') == 'docker_desktop'
+# cicd = os.environ.get('DJANGO_ENV') == 'cicd'
+# local = os.environ.get('DJANGO_ENV') == 'local'
+#
+# if docker_desktop:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'postgres',
+#             'USER': 'postgres',
+#             'PASSWORD': 'postgres',
+#             'HOST': 'pgdb',
+#             'PORT': '5432',
+#         }
+#     }
+# elif cicd:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.environ.get('POSTGRES_DB'),
+#             'USER': os.environ.get('POSTGRES_USER'),
+#             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#             'HOST': 'pgdb',
+#             'PORT': '5432',
+#         }
+#     }
+# elif local:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'purbeurre',
+#             'USER': 'postgres',
+#             'PASSWORD': 'postgre',
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#             'OPTIONS': {'sslmode': 'disable'},
+#         }
+#     }
+# else:
+#     raise ValueError("Invalid DJANGO_ENV value")
 
-if docker_desktop:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': 'pgdb',
-            'PORT': '5432',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'pgdb',
+        'PORT': 5432,
     }
-elif cicd:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_DB'),
-            'USER': os.environ.get('POSTGRES_USER'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': 'pgdb',
-            'PORT': '5432',
-        }
-    }
-elif local:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'purbeurre',
-            'USER': 'postgres',
-            'PASSWORD': 'postgre',
-            'HOST': 'localhost',
-            'PORT': '5432',
-            'OPTIONS': {'sslmode': 'disable'},
-        }
-    }
-else:
-    raise ValueError("Invalid DJANGO_ENV value")
+}
+
+
+
+
 
 
 # Password validation
